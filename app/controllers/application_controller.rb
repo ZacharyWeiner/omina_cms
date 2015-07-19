545cbaaa7b20968d43ce148f169eb88a39ec628f
ZacharyWeiner <zach@cloud.com>
2015-07-19 02:21:02 -0400

    added blocks and jumbotron sections to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 	before_action :get_navigation_pages
+	before_action :get_jumbotron
+	before_action :get_home_blocks
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -8,4 +10,12 @@ private
   def get_navigation_pages
 	@navigation_pages = Page.where(menu_display: true).where(is_published: true).order(:order)
   end
+
+    def get_jumbotron
+		@jumbotron = Block.where(position: 'jumbotron')
+	end 
+
+	def get_home_blocks
+		@blocks = Block.where(position: 'block').order(order: :asc)
+	end
 end
